8e8d2ca0038dfd236a7f3537e408a9be81921385
Joonsun Ryu <rjs7289@gmail.com>
2025-06-27 18:27:41 +0900

    Section02 - Global Layout, Pages Layout

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,36 +2,23 @@
  * 모든 페이지 역할을 하는 컴포넌트들의 부모 컴포넌트
  */
 
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
 
-  const onClickButton = () => {
-    router.push("/test");
-  };
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: NextPageWithLayout;
+}) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
-  useEffect(() => {
-    router.prefetch("/test");  // test 페이지에 대한 pre-fetching이 이루어짐
-  }, []);
-
-  return (
-    <>
-      <header>
-        <Link href={"/"}>index</Link>
-        &nbsp;
-        <Link href={"search"} prefetch={false}>search</Link>
-        &nbsp;
-        <Link href={"book/1"}>book/1</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-        </div>
-      </header>
-      <Component {...pageProps} />
-    </>
-  );
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,4 +1,6 @@
+import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
 export default function Page() {
   const router = useRouter();
@@ -6,4 +8,8 @@ export default function Page() {
   const { q } = router.query;
 
   return <h1>Search {q}</h1>;
+}
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>
 }
